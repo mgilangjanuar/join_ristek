556aee737902643d28cb2cf04cc419b684a478f6
jundialwan <jundi.alwan@gmail.com>
2016-01-07 20:18:28 +0700

    edit temporary base_url in config.php

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://ristek.cs.ui.ac.id/';
+$config['base_url'] = 'http://ristek.cs.ui.ac.id/_tmp/join_ristek/';
 
 /*
 |--------------------------------------------------------------------------
