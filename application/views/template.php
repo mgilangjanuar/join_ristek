c59ab0527e84a0d79ffa8ae143d2e02d894cb9f7
jundialwan <jundi.alwan@gmail.com>
2016-01-07 20:30:28 +0700

    switch to temporary assets link for local development

--- application/views/template.php
+++ application/views/template.php
@@ -8,17 +8,20 @@
 
 		<title>OPEN RECRUITMENT PORTAL - RISTEK FASILKOM UI</title>
 
-		<link rel="stylesheet" href="<?= site_url('assets/css/bootstrap.min.css') ?>">
+		<!-- <link rel="stylesheet" href=" //site_url('assets/css/bootstrap.min.css')"> -->
+		<link rel="stylesheet" href="http://localhost/GitRepo/join_ristek/assets/css/bootstrap.min.css">
 	</head>
 	<body>
 		<div class="container">
 			<div class="row">
-				<div class="col-md-4">.col-md-4</div>
+				<div class="col-md-4"><p></p></div>
 			    <div class="col-md-4">.col-md-4</div>
-				<div class="col-md-4">.col-md-4</div>
+				<div class="col-md-4"><p></p></div>
 			</div>
 		</div>
-		<script type="text/javascript" src="<?= site_url('assets/js/jquery.min.js') ?>"></script>
-		<script type="text/javascript" src="<?= site_url('assets/js/bootstrap.min.js') ?>"></script>
+		<!--<script type="text/javascript" src="<//site_url('assets/js/jquery.min.js') ?>">//</script>-->
+		<script type="text/javascript" src="http://localhost/GitRepo/join_ristek/assets/js/jquery.min.js"></script>
+		<!--<script type="text/javascript" src="//site_url('assets/js/bootstrap.min.js') ?>"></script>-->
+		<script type="text/javascript" src="http://localhost/GitRepo/join_ristek/assets/js/bootstrap.min.js"></script>
 	</body>
 </html>
