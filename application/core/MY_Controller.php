f9e1f74a18091d5bdd431a0cf649a3050c4f17cd
jundialwan <jundi.alwan@gmail.com>
2016-01-23 10:31:53 +0700

    modified cofig files for productions

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -4,6 +4,7 @@ class MY_Controller extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 		$this->load->library('session');
+		$this->load->helper('url');
 	}
 
 	// render template.php and the content which user request
