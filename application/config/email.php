db33c7a1234f681dd51cc645acc648d70d078bb9
jundialwan <jundi.alwan@gmail.com>
2016-01-24 00:38:18 +0700

    delete unnecessary file

--- application/config/email.php
+++
@@ -1,8 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-# using Ristek GMail Account
-$config['smtp_host'] = 'smtp.gmail.com';
-$config['smtp_user'] = 'ristek.ui@gmail.com';
-$config['smtp_pass'] = 'nasipadang2016';
-$config['smtp_port'] = '465'; 
-$config['smtp_timeout'] = '10';
