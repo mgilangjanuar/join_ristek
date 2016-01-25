b2d7c3d025538491f2014521dd1d02b8c59b405e
jundialwan <jundi.alwan@gmail.com>
2016-01-25 20:51:45 +0700

    remove quote-row from div in home.php

--- application/views/home.php
+++ application/views/home.php
@@ -23,12 +23,12 @@
 						<img class="img-responsive center-block" src="http://ristek.cs.ui.ac.id/img/logo-blue.png" alt="logo-ristek" width="90" height="120">
 						<h3 class="headline content-font">Ristek Fasilkom UI</h3>				
 						<h2 class="headline head-font"><b>EXPLORING TECHNOLOGY</b></h2>
-						<h4 class="headline content-font">Open Recruitment <br>SIG Junior Member &amp; Supporting Division Staff</h4>				
 					</div>					
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-md-6">
+					<h4 class="headline content-font">Open Recruitment <br>SIG Junior Member &amp; Supporting Division Staff</h4>				
 					<div class="row">
 						<div class="table-responsive">
 							<table class="table borderless SIG-table col-md-10">
@@ -90,7 +90,7 @@
 						</div>
 					</div>
 				</div>
-				<div class="col-md-6 quote-row">
+				<div class="col-md-6">
 					<div class="row">
 						<div class="col-md-12 col-md-offset-1">
 							<span class="content-font quote"><i>“Yesterday I was clever, so I wanted to change the world. Today I am wise, so I am changing myself” <br>-Rumi</i></span>	
