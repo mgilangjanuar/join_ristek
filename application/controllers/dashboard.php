3ecaaecc978021850c9ffe6e268d458626b2c803
jundialwan <jundi.alwan@gmail.com>
2016-01-25 08:50:20 +0700

    remove debug mode: admin can access dashboard

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -21,7 +21,7 @@ Class Dashboard extends MY_Controller {
 			$user = $this->session->userdata('userdata');
 
 			// if admin redirect to admin page
-			// if ($this->isAdmin($user['username'])) redirect(site_url('admin747835'));
+			if ($this->isAdmin($user['username'])) redirect(site_url('admin747835'));
 
 			// check if user registered or not
 			if ($this->isRegistered($user['username'])) {
