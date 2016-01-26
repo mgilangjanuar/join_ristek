ca8416f47b150502d0c7ccddafa52427fb985529
jundialwan <jundi.alwan@gmail.com>
2016-01-26 17:13:46 +0700

    secure date

--- application/views/home.php
+++ application/views/home.php
@@ -125,7 +125,7 @@
 								<?php endif; ?>
 								<p> Buka <?= strtotime('26th January 2016 19:00:00') ?></p>
 								<p><?= strtotime('2016-01-26 16:55:00.0') ?></p>
-								<p><?= strtotime('2016-01-26 17:09:00.0') ?></p>
+								<p>today 5:13 pm<?= strtotime('2016-01-26 17:13:00.0') ?></p>
 								<p> Sekarang <?= strtotime('now') ?></p>
 
 								<div class="col-md-4 col-md-offset-2 text-center ">					
