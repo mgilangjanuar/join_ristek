48c3a0051b6795fdbb3fe6f684a38974477eedde
Muhammad Gilang Januar <mgilangjanuar@gmail.com>
2016-01-26 21:21:05 +0700

    fix blank dashboard

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -270,7 +270,7 @@
 	  		<div class="col-md-12">
 	  			<div class="row">
 		  			<div class="col-md-12">
-				 		<?php if(time() < (strtotime('8th February 2016 19:30:00.0')-(21*60+30))) | ($isUserQualified->isPil1Qualified == 0 & $isUserQualified->isPil2Qualified == 0)): ?>
+				 		<?php if(time() < (strtotime('8th February 2016 19:30:00.0')-(21*60+30)) | ($isUserQualified->isPil1Qualified == 0 & $isUserQualified->isPil2Qualified == 0)): ?>
 				 		<br>
 				 		<div class="well well-lg content-font">Masa Wawancara belum dibuka atau Anda tidak lolos seleksi tugas. <br><b>Masa Wawancara adalah <u>9-14 Februari 2016</u></b></div>
 				 		<?php else: ?>
